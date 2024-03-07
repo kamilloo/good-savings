779017a020787704733ffa5bbc54e599a7214ab3
kamilloo <kamil.pietka85@gmail.com>
2024-03-07 20:27:13 +0100

    add nestjs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BalanceController } from './balance/balance.controller';
@@ -14,7 +15,7 @@ import { TradeService } from './trades/trade.service';
 import { BotService } from './bot/BotService';
 
 @Module({
-  imports: [TradingModule],
+  imports: [TradingModule, ConfigModule.forRoot()],
   controllers: [
     AppController,
     BalanceController,
